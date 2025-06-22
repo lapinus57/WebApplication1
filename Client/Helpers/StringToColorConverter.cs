c9e0cb1a5095683843a133dde353887cdabc7ebb
benoit <the-gollum@hotmail.fr>
2025-06-22 07:00:52 +0200

    #220625/0700

--- Client/Helpers/StringToColorConverter.cs
+++ Client/Helpers/StringToColorConverter.cs
@@ -15,10 +15,10 @@ namespace Client.Helpers
             {
                 try
                 {
-                    return ColorUtils.FromHex(s);
                     var color = ColorUtils.FromHex(s);
-                    if (targetType == typeof(Brush))
+                    if (targetType == typeof(Brush) || targetType == typeof(SolidColorBrush))
                         return new SolidColorBrush(color);
+
                     return color;
                 }
                 catch
@@ -26,21 +26,23 @@ namespace Client.Helpers
                 }
                
             }
-            if (targetType == typeof(Brush))
+            if (targetType == typeof(Brush) || targetType == typeof(SolidColorBrush))
                 return new SolidColorBrush(Colors.Black);
             return Colors.Black;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
-            if (value is SolidColorBrush brush)
-            {
-                return ColorUtils.ToHex(brush.Color);
-            }
             if (value is Color color)
             {
                 return ColorUtils.ToHex(color);
             }
+
+            if (value is SolidColorBrush brush)
+            {
+                return ColorUtils.ToHex(brush.Color);
+            }
+
             return "#000000";
         }
     }
