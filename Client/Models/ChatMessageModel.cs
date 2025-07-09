aa1a86c1897ae67284f48bdd0a40882a90715e5e
lapinus57 <benoit.muller.57@gmail.com>
2025-07-09 16:58:37 +0200

    Add context menu for messages and logical delete

--- Client/Models/ChatMessageModel.cs
+++ Client/Models/ChatMessageModel.cs
@@ -4,12 +4,14 @@ namespace Client.Models
 {
     public class ChatMessageModel
     {
+        public int Id { get; set; }
         public string Sender { get; set; } = string.Empty;
         public string Destinataire { get; set; } = string.Empty;
         public string Room { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public string Avatar { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; }
+        public bool IsDeleted { get; set; }
 
         public string TimeFormatted => Timestamp.ToString("dd/MM/yy HH:mm");
         public string Header => $"{Sender} ({Room}) :";
